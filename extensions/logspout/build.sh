d3058c9d5e26079fc7a725e107e25f3477b18524
AVB <abdddev@gmail.com>
2024-12-01 01:22:14 +1000

    added grafana/prometheus + ELK

---
+++ extensions/logspout/build.sh
@@ -0,0 +1,8 @@
+set -e
+apk add --update go build-base git mercurial ca-certificates
+cd /src
+go build -ldflags "-X main.Version=$1" -o /bin/logspout
+apk del go git mercurial build-base
+rm -rf /root/go /var/cache/apk/*
+
+ln -fs /tmp/docker.sock /var/run/docker.sock
